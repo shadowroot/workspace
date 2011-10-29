fa9833862212ffe0b226d1e08bf92aaeb4cbc423
jonnyATroot <shadow.to.root@gmail.com>
2011-10-29 03:23:36 +0200

    Save so 29.10.2011

--- set.bat
+++ set.bat
@@ -2,5 +2,11 @@ git config --global user.name "jonnyATroot"
 git config --global user.email "shadow.to.root@gmail.com"
 git add *
 git commit -a -m "Save %DATE%"
+cd ..
+git add *
+git commit -a -m "Save %DATE%"
+git push --all sf
+yes
+jonny555
 pause
 exit
--- workspace.bat
+++ workspace.bat
@@ -4,4 +4,5 @@ start %COMSPEC% /c C:\Users\Jonny\Downloads\eclipse\eclipse.exe
 start %COMSPEC% /c "C:\Program Files (x86)\AIMP3\AIMP3.exe"
 start %COMSPEC% /c "C:\Program Files (x86)\Mozilla Firefox\firefox.exe"
 start %COMSPEC% /c "C:\totalcmd\TOTALCMD.EXE"
+start %COMSPEC% /c "notepad"
 exit
