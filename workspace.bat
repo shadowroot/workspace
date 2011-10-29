e5dee873532fbb27df090fcf9942dcdd18f0e569
jonnyATroot <shadow.to.root@gmail.com>
2011-10-29 13:38:26 +0200

    Save so 29.10.2011

--- workspace.bat
+++ workspace.bat
@@ -5,4 +5,5 @@ start %COMSPEC% /c "C:\Program Files (x86)\AIMP3\AIMP3.exe"
 start %COMSPEC% /c "C:\Program Files (x86)\Mozilla Firefox\firefox.exe"
 start %COMSPEC% /c "C:\totalcmd\TOTALCMD.EXE"
 start %COMSPEC% /c "notepad"
+start %COMSPEC% /c "D:\miranda-im-v0.9.32-x64\miranda64.exe"
 exit
