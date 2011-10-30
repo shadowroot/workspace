3b2571da4c1952d6b949b00a3db828affd3208ae
jonnyATroot <shadow.to.root@gmail.com>
2011-10-30 21:13:00 +0100

    Save ne 30.10.2011

--- workspace.bat
+++ workspace.bat
@@ -6,4 +6,5 @@ start %COMSPEC% /c "C:\Program Files (x86)\Mozilla Firefox\firefox.exe"
 start %COMSPEC% /c "C:\totalcmd\TOTALCMD.EXE"
 start %COMSPEC% /c "notepad"
 start %COMSPEC% /c "D:\miranda-im-v0.9.32-x64\miranda64.exe"
+start %COMSPEC% /c "C:\Program Files (x86)\xchat\xchat.exe"
 exit
